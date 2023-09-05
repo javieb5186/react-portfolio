26bb51cfcc7ab86d0d57ecafe69f892437016d8f
javieb5186 <javieb5186@gmail.com>
2023-09-05 10:39:18 -0600

    Add pic

--- portfolio/src/components/home.js
+++ portfolio/src/components/home.js
@@ -1,8 +1,15 @@
 import React from "react";
 
+const style = {
+  backgroundImage: 'url(' + '/profileT.png' + ')',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'center',
+  backgroundSize: '35%',
+}
+
 function Home() {
   return (
-    <div className='sec row d-flex align-items-center'>
+    <div className='sec row d-flex align-items-center' style={style}>
       <div className='col'>
         <p className='display-1 text-center'>Javier B</p>
         <p className='display-6 text-center'>Game Developer, Full Stack Developer, and App Developer</p>
